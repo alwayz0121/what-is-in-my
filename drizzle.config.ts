aba511408de4afd8a72177ab9390d689b77808bd
alwayz0121 <alwayz0121@gmail.com>
2025-09-03 02:06:58 +0900

    db : drizzle orm install

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./app/**/schema.ts",
+  out: "./app/migrations",
+    dialect: "postgresql",
+    dbCredentials: {
+      url: process.env.DATABASE_URL!,
+    },
+  },
+);
+  
+  
+  
