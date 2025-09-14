49601edaad01bfde737daf29709fb6f889dc4529
alwayz0121 <alwayz0121@gmail.com>
2025-09-14 16:30:08 +0900

    chore: supa-client 연결

--- app/supa-client.ts
+++ app/supa-client.ts
@@ -1,6 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
+import type { Database } from "../database.types";
 
-export const browserClient = createBrowserClient(
+export const browserClient = createBrowserClient<Database>(
   import.meta.env.VITE_SUPABASE_URL!,
   import.meta.env.VITE_SUPABASE_ANON_KEY!
 );
